b08e75d1ac7b30c270f236f6ab9b6ca37e132540
Eduardo Sánchez <eduardosr1599@gmail.com>
2022-10-30 22:28:20 -0400

    Add contact section

---
+++ src/contact.js
@@ -0,0 +1,55 @@
+import createLayout from "./element";
+import phoneImage from "./phone_icon.svg";
+import pacificOceanImage from "./pacific_ocean.png";
+import locationImage from "./location_icon.svg";
+import mailIcon from "./mail_icon.svg";
+
+function createContactSection(){
+    let contactSection=createLayout().createElement("section");
+
+    let createGroup=(textContent,image)=>{
+        let div=createLayout().createElement("div");
+        let text=createLayout().createElement("p",textContent);
+        let icon=createLayout().createElement("img");
+        icon.setAttribute("src",image);
+        div.append(icon,text);
+        return div;    
+    }
+    let tlf=createGroup("(555)-555-5555",phoneImage);
+    let location=createGroup("831 Bottomfeeder Lane, Bikini Bottom, Pacific Ocean",locationImage);
+    let message=createGroup("Message us",mailIcon);
+
+    let createFormElement=(type,id,labelText)=>{
+        let div=createLayout().createElement("div");
+        let label=createLayout().createElement("id",labelText);
+        label.setAttribute("for",id);
+        let input=createLayout().createElement("input");
+        input.setAttribute("type",type);
+        input.setAttribute("id",id);
+        if (type==="textarea"){input=createLayout().createElement("textarea")};
+        div.append(label,input);
+        return div;
+    };
+
+    let nameInput=createFormElement("text","name","Full name");
+    let emailInput=createFormElement("email","email","E-mail");
+    let textArea=createFormElement("textarea","message","Write your message");
+
+    let button=createLayout().createElement("button","Submit");
+    button.setAttribute("type","button");
+    let form=createLayout().createElement("form");
+    form.append(nameInput,emailInput,textArea,button);
+
+    let pacificOcean=createLayout().createElement("img");
+    pacificOcean.setAttribute("src",pacificOceanImage);
+    contactSection.append(tlf,location,message,form,pacificOcean);
+
+    let render=function(){
+        contactSection.classList.add("contact");
+        document.querySelector("main").appendChild(contactSection);
+    };
+    return {
+        render
+    }
+};
+export default createContactSection;
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import "./style.css";
 import createHomeSection from "./home";
 import createPoliciesSection from "./policies";
 import createMenuSection from "./menu";
+import createContactSection from "./contact";
 
 let content=document.querySelector("#content");
 content.append(createLayout().header,createLayout().tabsContainer,createLayout().main);
@@ -20,20 +21,23 @@ function removeContent(){
     })
 };
 
-// let tabs=document.querySelectorAll("div>ul>li");
-// tabs.forEach(tab=>{
-//     tab.addEventListener("click",removeContent);
-// });
 let menuTab=document.querySelector("div>ul>li:nth-of-type(2)");
 menuTab.addEventListener("click",()=>{
     removeContent();
-    menuTab.classList.add("active")
+    menuTab.classList.add("active");
     createMenuSection().render();
-})
+});
 
 let policiesTab=document.querySelector("div>ul>li:last-of-type");
 policiesTab.addEventListener("click",()=>{
     removeContent();
     policiesTab.classList.add("active");
     createPoliciesSection().render();
+});
+
+let contactTab=document.querySelector("div>ul>li:nth-of-type(3)");
+contactTab.addEventListener("click",()=>{
+    removeContent();
+    contactTab.classList.add("active");
+    createContactSection().render();
 })
